66b35607a1085a360632855f1c67d9f08e6e885e
Elijah Gabriel <egabrie2@stumail.jccc.edu>
2022-05-14 14:02:52 -0500

    Javascript For Bread Empire

---
+++ source.js
@@ -0,0 +1,25 @@
+// Kansasian - Elijah Gabriel
+
+// Learning how to create changing text based on a timer. Work-in-progress.
+
+// function textChange ( ) {
+//     let myArray = ['"The simplicity is just remarkable." - Mark', '"Tastes just like bread should" - Jacklyn', '"Kansas\'s greatest bread recipe." - Victoria'];
+//     let praise = document.getElementById('praise-revolve');
+//     let text = myArray[0];
+
+
+//     if (text == myArray[0]) {
+//         text = myArray[1];
+//         setTimeout(textChange, 5000)
+//         console.log('myArray[1]');
+//     } else {
+//         text = myArray[2];
+//         setTimeout(textChange, 5000)
+//         console.log('myArray[2]');
+//     }
+//     praise.innerHTML = text;
+//     setTimeout(textChange, 5000)
+// }
+
+
+// textChange();
